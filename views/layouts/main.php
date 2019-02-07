7245bc1c78e3ec6c6ee5341f4a0d50120667dd96
Ziyat <termit_90@mail.ru>
2019-02-07 15:48:35 +0500

    fixed

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,7 +15,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+    <meta name="yandex-verification" content="aa1e3a1929ac7316" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
