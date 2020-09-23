d1a13640e5f040717aa48a5589a7bdcb9a5fd7e4
Ziyat <termit_90@mail.ru>
2020-09-24 02:06:01 +0500

    added vendor.zip and madetec.uz_dump.sql

---
+++ madetec.uz_dump.sql
@@ -0,0 +1,165 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `is_admin` tinyint(1) DEFAULT '0',
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `idx-users-status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `views` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `article` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `old_price` float DEFAULT NULL,
+  `params` text COLLATE utf8_unicode_ci,
+  `quantity` float DEFAULT NULL,
+  `main_photo_id` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT '20',
+  `views` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk-products-category_id` (`category_id`),
+  CONSTRAINT `fk-products-category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `category_assignments` (
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`,`category_id`),
+  KEY `idx-category_assignments-product_id` (`product_id`),
+  KEY `idx-category_assignments-category_id` (`category_id`),
+  CONSTRAINT `fk-category_assignments-category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk-category_assignments-product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address_line_1` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address_line_2` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_of_birth` int(11) DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `params` text COLLATE utf8_unicode_ci,
+  `avatar` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` int(11) DEFAULT '20',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE `dealer_assignments` (
+  `dealer_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `percent` float DEFAULT NULL,
+  PRIMARY KEY (`dealer_id`,`client_id`),
+  UNIQUE KEY `idx-dealer_assignments-dealer_id` (`dealer_id`,`client_id`),
+  KEY `fk-dealer_assignments-client_id` (`client_id`),
+  CONSTRAINT `fk-dealer_assignments-client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+  CONSTRAINT `fk-dealer_assignments-dealer_id` FOREIGN KEY (`dealer_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `discounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `photo` varchar(255) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) NOT NULL,
+  `text` text,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `expired_at` int(11) NOT NULL,
+  `published_at` int(11) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `text` text,
+  `slug` varchar(255) NOT NULL,
+  `main_photo_id` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `published_at` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `news_photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_id` int(11) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `sort` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx-news_photos-product_id` (`news_id`),
+  CONSTRAINT `fk-news_photos-product_id` FOREIGN KEY (`news_id`) REFERENCES `news` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `order_products` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` float DEFAULT NULL,
+  UNIQUE KEY `idx-order_products-product_id` (`order_id`,`product_id`),
+  KEY `fk-order_products-product_id` (`product_id`),
+  CONSTRAINT `fk-order_products-product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx-orders-client_id` (`client_id`),
+  CONSTRAINT `fk-orders-client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `sort` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx-photos-product_id` (`product_id`),
+  CONSTRAINT `fk-photos-product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `warranties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `expired_at` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx-warranties-client_id` (`client_id`),
+  CONSTRAINT `fk-warranties-client_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `warranty_assignments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `warranty_id` int(11) NOT NULL,
+  `params` text,
+  `created_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
